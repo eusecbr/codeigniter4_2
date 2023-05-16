ae2df0a302379e454d85ee0024e6877850ac8eba
Eusebio <eusecbr@hotmail.com>
2023-05-16 13:34:30 +0200

    codeigniter4

--- app/Controllers/controllerPortada.php
+++ app/Controllers/controllerPortada.php
@@ -7,7 +7,7 @@ class ControllerPortada extends BaseController
     public function index()
     {
         return  view('portada/cabecera').
-                view('portada_cuerpo').
+                view('portada/cuerpo').
                 view('portada/pie');
     }
 
