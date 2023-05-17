dfc2bca8c8661a86f28d6256919ee96df45f8477
Eusebio <eusecbr@hotmail.com>
2023-05-17 09:48:05 +0200

    codeigniter4

--- app/Controllers/ControllerInvestigadores.php
+++ app/Controllers/ControllerInvestigadores.php
@@ -33,14 +33,14 @@ class ControllerInvestigadores extends BaseController
 
     public function index()
     {
-        return view('portada/cabecera').
+        return  view('portada/cabecera').
                 view('investigadores/listado_investigadores').
                 view('portada/pie');
     }
 
     public function add()
     {
-        return view('portada/cabecera').
+        return  view('portada/cabecera').
                 view('investigadores/add_investigadores').
                 view('portada/pie');
     }
